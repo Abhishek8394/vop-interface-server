4ebc1a019e28555a73d9713eb5b2e93434d702ae
Abhishek Patel <abhidgreat8394@gmail.com>
2017-01-20 18:54:52 -0500

    Move connection handling to connection manager
    Organizing code. Connection handling done by a connection manager.

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ const https = require('https');
 const http = require('http');
 const express = require('express');
 const net = require('net');
+//  Custom libs
+const UTILITY = require('./lib/utility.js');
+const CONNECTION_MANAGER_LIB = require('./lib/ConnectionManager.js');
+const ConnectionTypes = CONNECTION_MANAGER_LIB.ConnectionTypes;
+const VOPConnection = CONNECTION_MANAGER_LIB.VOPConnection;
+const AppConnection = CONNECTION_MANAGER_LIB.AppConnection;
+
+var connectionsManager = new CONNECTION_MANAGER_LIB.ConnectionsManager();
+var utility = new UTILITY();
 // Settings -------------------------
-var useSecureServerWebSocket = false; // If websocket server should listen on secure/unsecure connection
-var webSocketPort = 8080; // Port websocket server listens to
-var privateKey = fs.readFileSync('cert/key.pem'); // for websocket server
+var useSecureServerWebSocket = false; 				// If websocket server should listen on secure/unsecure connection
+var webSocketPort = 8080; 							// Port websocket server listens to
+var privateKey = fs.readFileSync('cert/key.pem'); 	// for websocket server
 var certificate = fs.readFileSync('cert/cert.pem'); // for websocket server
-var TCP_SERVER_PORT_VE = 11917; // TCP connection port to be used by voice engine
-var TCP_SERVER_PORT_APPS = 12000; // TCP connection port for rest of the clients to connect to.
-var vopServerEncoding = "utf8"; // Interpret data from vop as a string of this encoding
-var appServerEncoding = "utf8"; // Interpret data from client apps as a string of this encoding
+var TCP_SERVER_PORT_VE = 11917; 					// TCP connection port to be used by voice engine
+var TCP_SERVER_PORT_APPS = 12050; 					// TCP connection port for rest of the clients to connect to.
+var vopServerEncoding = "utf8"; 					// Interpret data from vop as a string of this encoding
+var appServerEncoding = "utf8"; 					// Interpret data from client apps as a string of this encoding
 //  ---------------------------------
 var credentials = {key:privateKey,cert:certificate};
 var app = express();
@@ -59,7 +68,7 @@ wss2.on('connection', function connection(ws) {
 // returns same value as node js net.socket.write() method.
 function sendMessage(socket,msgObj,callback=null,asJson = true,encoding="utf8"){
 	if(asJson){		
-		return socket.write(getJSONString(msgObj),encoding,callback);
+		return socket.write(utility.getJSONString(msgObj),encoding,callback);
 	}
 	else{
 		return socket.write(msgObj,encoding,callback);
@@ -69,7 +78,7 @@ function sendMessage(socket,msgObj,callback=null,asJson = true,encoding="utf8"){
 // Handle message received by vop
 function vopMessageHandler(buffer){
 	console.log(buffer);
-	var buffData = jsonify(buffer);
+	var buffData = utility.jsonify(buffer);
 	if(buffData!=undefined){
 		if(buffData["msg"]=="hello"){
 			sendMessage(vopServSocket,"yo");
@@ -82,11 +91,12 @@ function appsMessageHandler(buffer){
 }
 
 // server for speech recognition module (called vop) ----------------------
-var vopServSocket;
+var vopServSocket;var vopconobj ;
 var vopServer = net.createServer((socket)=>{
 	vopServSocket = socket;
 	vopServSocket.setEncoding(vopServerEncoding);
-	socket.on('data',vopMessageHandler);
+	vopconobj = connectionsManager.newConnectionHandler(vopServSocket,ConnectionTypes.VOP);
+	socket.on('data',function(b){vopconobj.read(b);});
 	console.log("vop connected");
 });
 vopServer.on('error',function(err){
@@ -100,41 +110,20 @@ vopServer.listen(TCP_SERVER_PORT_VE,function(){
 // Server for other applications --------------------------------
 var appServSockets = [];
 var otherAppServer = net.createServer((s)=>{
-	var tmpName = generateTempName();
+	// var tmpName = utility.generateTempName();
+	// appServSockets.push({tmpName:{"socket":s}});
 	s.setEncoding(appServerEncoding);
-	appServSockets.push({tmpName:{"socket":s}});
-	console.log("new app connected");
+	var aco = connectionsManager.newConnectionHandler(s,ConnectionTypes.APP);
+	// console.log("foo "+connectionsManager.VOPConnection);
+	console.log("new app connected: "+aco.getName());
 });
-vopServer.on('error',function(err){
+otherAppServer.on('error',function(err){
 	console.log("app server: error connecting");
 });
-vopServer.listen(TCP_SERVER_PORT_APPS,function(){
+otherAppServer.listen(TCP_SERVER_PORT_APPS,function(){
 	console.log("app server listening on "+TCP_SERVER_PORT_APPS);
 });
 //  -------------------------------------------------------------
 
-function generateTempName(){
-	return "temp" + getRandomInt(1,10000)+"-"+getRandomInt(10000,100000);
-}
 
-// Random Integer generator
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-function getJSONString(obj){
-	return JSON.stringify(obj,null,'\t');
-}
-function jsonify(buff){
-	var jb;
-	try{
-		jb = JSON.parse(buff);
-	}
-	catch(e){
-		console.log("jsonify error");
-		console.log(e);
-		jb =  undefined;
-	}
-	return jb;
-}
+
